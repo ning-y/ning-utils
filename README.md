0a65ba982e24b7e0372a92182d1272c6719407bc
ning <ningyuan.sg@gmail.com>
2021-10-07 20:53:06 +0800

    Add pdfaddpw.py

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 A collection of convenience scripts.
 
-- [**validate-gs-checksums.py**](https://github.com/ning-y/ning-utils/blob/master/validate-gs-checksums.py) checks of two files are the same, based on their CRC32c hash as computed by [gsutil](https://cloud.google.com/storage/docs/gsutil/commands/hash).
+- [**pdfaddpw.py**](pdfaddpw.py) password protects PDF files in-place.
+- [**validate-gs-checksums.py**](validate-gs-checksums.py) checks of two files are the same, based on their CRC32c hash as computed by [gsutil](https://cloud.google.com/storage/docs/gsutil/commands/hash).
   The files can be local or remote on Google Cloud Storage.
-
---
+++ pdfaddpw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse, getpass, subprocess
+
+parser = argparse.ArgumentParser(
+    prog="pdfaddpw", description="Password protect PDFs")
+parser.add_argument("pdfs", nargs="+")
+args = parser.parse_args()
+
+password = getpass.getpass()
+
+for pdf in args.pdfs:
+    # Without -dNOPAUSE, user must hit <RET> each page.
+    # Without -DBATCH, gs remains in REPL mode after job done
+    ps_gs = subprocess.Popen([
+            "gs", "-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite",
+            "-dQUIET", "-sOutputFile=-",
+            f"-sOwnerPassword={password}", f"-sUserPassword={password}", pdf],
+        stdout=subprocess.PIPE)
+    ps_sponge = subprocess.Popen(["sponge", pdf], stdin=ps_gs.stdout)
+    ps_sponge.wait()
